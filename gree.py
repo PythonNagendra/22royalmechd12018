03020d8c902021260c0edda11c9f18f59697c126
PythonNagendra <42933090+PythonNagendra@users.noreply.github.com>
2018-09-17 16:31:05 +0530

    Add files via upload

---
+++ gree.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+if(a>b&a>c):
+	print("a is the greatest number")
+elif(b>c):
+	print("b is the greatest number")
+else:
+	print("c is the greatest number")
---
+++ marks.py
@@ -0,0 +1,15 @@
+marks=int(input("enter your marks:"))
+if(marks>90):
+	print("A+")
+elif(marks>80):
+	print("A")
+elif(marks>70):
+	print("B+")
+elif(marks>60):
+	print("B")
+elif(marks>40):
+	print("C")
+elif(marks<40):
+	print("fail")
+
+
