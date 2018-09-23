ba2b5cac5b02318335dce7e48891c222c5cc6c50
PythonNagendra <42933090+PythonNagendra@users.noreply.github.com>
2018-09-23 14:24:38 +0530

    Update gree.py

--- gree.py
+++ gree.py
@@ -1,3 +1,4 @@
+Input
 a=int(input("enter the value of a:"))
 b=int(input("enter the value of b:"))
 c=int(input("enter the value of c:"))
@@ -7,3 +8,10 @@ elif(b>c):
 	print("b is the greatest number")
 else:
 	print("c is the greatest number")
+
+
+Output
+Enter the value of a=20
+Enter the value of b=30
+Enter the value of c=50
+c is the greatest number
